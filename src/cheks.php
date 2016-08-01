08eaf9e4fe3f07c0bea9f47e01c2358f2e23ce60
rabadan731 <rabadan731@yandex.ru>
2016-08-01 13:09:25 +0300

    step2

--- src/cheks.php
+++ src/cheks.php
@@ -5,10 +5,13 @@ namespace hexletPsrLinter;
 function checkFunctionName($name)
 {
     $regex = "^[a-z]+([A-Z]?[a-z]+)+$";
+
     $result = preg_match_all("/$regex/", $name);
+
     if ($result == 0) {
         return false;
-    } else {
-        return true;
     }
+
+    return true;
+
 }
