4d01f39d044e0a4b5362a415ecb228890530139c
rabadan <rabadan731@yandex.ru>
2016-08-04 10:00:46 +0300

    refractoring code

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -14,45 +14,23 @@ class LinterTest extends \PHPUnit_Framework_TestCase
             __DIR__."/fixtures/good/good.php"=>[],
         ]);
 
-        $result = $linter->run(["path"=>__DIR__."/fixtures/bad"]);
-        $this->assertArrayHasKey(__DIR__."/fixtures/bad/bad.php", $result);
-        $this->assertContains(3, $result[__DIR__."/fixtures/bad/bad.php"][0]);
-        $this->assertContains("Name", $result[__DIR__."/fixtures/bad/bad.php"][0]);
-        $this->assertContains(8, $result[__DIR__."/fixtures/bad/bad.php"][1]);
-        $this->assertContains("NameGood", $result[__DIR__."/fixtures/bad/bad.php"][1]);
-        $this->assertContains(13, $result[__DIR__."/fixtures/bad/bad.php"][2]);
-        $this->assertContains("bad_bad_name", $result[__DIR__."/fixtures/bad/bad.php"][2]);
+        $result = $linter->run(["path"=>__DIR__."/fixtures/bad/bad.php"]);
+
+        $this->assertNotEquals($result, [
+            __DIR__."/fixtures/bad/bad.php"=>[],
+        ]);
     }
 
     public function testLint()
     {
         $linter = new Linter();
 
-        $codeGood = "<?php 
-        function goodName() {
-        }
-        function goodname() {
-        } 
-        function goodNameName() {
-        } ";
+        $codeGood = file_get_contents(__DIR__."/fixtures/good/good.php");
 
         $this->assertEquals($linter->lint($codeGood), []);
 
-        $codeGood = "<?php 
-        function BadName() {
-        }
-        function bad_name() {
-        } 
-        function BadNameName() {
-        } ";
+        $codeBad = file_get_contents(__DIR__."/fixtures/bad/bad.php");
 
-        $linter = new Linter();
-        $result = $linter->lint($codeGood);
-        $this->assertContains(2, $result[0]);
-        $this->assertContains("BadName", $result[0]);
-        $this->assertContains(4, $result[1]);
-        $this->assertContains("bad_name", $result[1]);
-        $this->assertContains(6, $result[2]);
-        $this->assertContains("BadNameName", $result[2]);
+        $this->assertNotEquals($linter->lint($codeBad), []);
     }
 }
